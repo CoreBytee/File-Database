ce7fa6f0f515fd12cf1b04983e3650789e1dd210
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-22 21:28:35 +0100

    send to server

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,13 +2,14 @@ export const Login = () => {
     return (
         <div class="Login">
             <a class="header">FILEDB</a>
-            <form hx-post   >
+            <form hx-post="/api/v1/htmx/login" hx-target=".error" hx-swap="innerHTML">
                 <label for="username">Username</label>
                 <input type="text" name="username" id="username" />
                 <label for="password">Password</label>
                 <input type="password" name="password" id="password" />
                 <input type="submit" value="Login" />
             </form>
+            <a class="error"></a>
         </div>
     )
 }
