65495b30e1fb25b45d80cdd1fd4cd192c4bbd1b6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-23 11:03:38 +0100

    Create GalleryPage.jsx

---
+++ src/Layouts/GalleryPage.jsx
@@ -0,0 +1,12 @@
+import { Root } from "../Components/Root"
+import { Center } from "../Components/Center"
+
+export default function GalleryPage({ children }) {
+    return (
+        <Root>
+            <Center viewport>
+                
+            </Center>
+        </Root>
+    )
+}
