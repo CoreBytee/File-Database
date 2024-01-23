c54c2ff117eace176f52f555ceb797b4ee9590e7
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-23 15:55:03 +0100

    Update Topbar.jsx

--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -1,15 +1,12 @@
-export default function Topbar() {
+import ModeSwitch from "./ModeSwitch.jsx"
+
+export default function Topbar( { username } ) {
+    console.log(username)
     return (
-        <div class="Login">
-            <a class="header">FILEDB</a>
-            <form hx-post="/api/v1/htmx/login" hx-target=".error" hx-swap="innerHTML">
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" />
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" />
-                <input type="submit" value="Login" />
-            </form>
-            <a class="error"></a>
+        <div class="Topbar">
+            <ModeSwitch />
+            <p class="header">FILEDB</p>
+            <p hx-post="/api/v1/htmx/logout" class="logoutbutton">logout from {username}</p>
         </div>
     )
 }
