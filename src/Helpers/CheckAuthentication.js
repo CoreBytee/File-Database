5955c1a8e2d437062a315c02581a6cafdabf2d64
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-23 23:23:34 +0100

    Update CheckAuthentication.js

--- src/Helpers/CheckAuthentication.js
+++ src/Helpers/CheckAuthentication.js
@@ -1,3 +1,5 @@
 export default async function CheckAuthentication(Request) {
+    const TokenData = await Request.JWT.verify(Request.cookie.authentication.value)
+    if (!TokenData) return false
     return await Request.JWT.verify(Request.cookie.authentication.value)
 }
