836c3e1bc146c76c7757bd5f01f78c826a747c52
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-23 15:55:14 +0100

    username

--- src/Layouts/GalleryPage.jsx
+++ src/Layouts/GalleryPage.jsx
@@ -1,9 +1,11 @@
 import Root from "../Components/Root"
 import Center from "../Components/Center"
+import Topbar from "../Components/Topbar"
 
-export default function GalleryPage({ children }) {
+export default function GalleryPage({ children, username }) {
     return (
         <Root>
+            <Topbar username={username} />
             <Center viewport>
                 
             </Center>
--- src/Layouts/LoginPage.jsx
+++ src/Layouts/LoginPage.jsx
@@ -2,11 +2,11 @@ import Root from "../Components/Root"
 import Login from "../Components/Login"
 import Center from "../Components/Center"
 
-export default function LoginPage({ children }) {
+export default function LoginPage({ children, username }) {
     return (
         <Root>
             <Center viewport>
-                <Login />
+                <Login username={username} />
             </Center>
         </Root>
     )
