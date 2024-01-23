95e9de571d74452cc317dc062a02b7093cabb483
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-23 15:55:01 +0100

    icons

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -17,6 +17,9 @@ export default function Root({ children }) {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet" />
+
+                {/* Icons */}
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
             </head>
             <body>
                 {children}
