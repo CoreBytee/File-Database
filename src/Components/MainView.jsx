5e44db42a821b4c00978642d6ef4f4e33a5dd87a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-24 16:02:20 +0100

    work on tabselector

---
+++ src/Components/MainView.jsx
@@ -0,0 +1,15 @@
+import TabSelector from "./TabSelector";
+
+export default function MainView() {
+    return (
+        <div class="MainView">
+            <div class="content">
+
+            </div>
+            <div class="sidebar">
+                <TabSelector />
+            </div>
+
+        </div>
+    )
+}
---
+++ src/Components/TabSelector.jsx
@@ -0,0 +1,11 @@
+export default function TabSelector() {
+    return (
+        <div class="TabSelector">
+            <p>Current View:</p>
+            <select name="" id="">
+                <option value="">Gallery</option>
+                <option value="">Settings</option>
+            </select>
+        </div>
+    )
+}
