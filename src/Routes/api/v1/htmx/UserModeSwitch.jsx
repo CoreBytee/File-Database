a03929fafbc8bf6bfe2060306107cb5e0c586022
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-24 12:38:46 +0100

    Update UserModeSwitch.jsx

--- src/Routes/api/v1/htmx/UserModeSwitch.jsx
+++ src/Routes/api/v1/htmx/UserModeSwitch.jsx
@@ -4,7 +4,6 @@ export default function Login(FileDB, App) {
     App.post(
         "/api/v1/htmx/usermodeswitch",
         async (Request) => {
-            console.log(Request.body)
             return <ModeSwitch state={Request.body.usermodeswitch=="1"} />
         }
     )
