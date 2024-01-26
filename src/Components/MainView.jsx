5d6d694416c7c08619ad4eef4d43b2e19fa61083
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-26 16:46:56 +0100

    load

--- src/Components/MainView.jsx
+++ src/Components/MainView.jsx
@@ -4,7 +4,7 @@ export default function MainView() {
     return (
         <div class="MainView">
             <div class="content">
-
+                <div hx-post="/api/v1/htmx/content" hx-trigger="load" hx-swap="outerHTML"></div>
             </div>
             <div class="sidebar">
                 <TabSelector />
