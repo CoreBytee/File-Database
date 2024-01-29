c13deba9fc414879d635fd6b379b149ec8200b22
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-29 14:21:02 +0100

    Update TabSelector.jsx

--- src/Components/TabSelector.jsx
+++ src/Components/TabSelector.jsx
@@ -2,7 +2,7 @@ export default function TabSelector() {
     return (
         <div class="TabSelector">
             <p>Current View:</p>
-            <select name="pagename" id="" hx-post="/api/v1/htmx/content" hx-target=".content">
+            <select name="pagename" id="" hx-post="/api/v1/htmx/content" hx-target=".content" hx-include="[name='usermodeswitch']">
                 <option value="gallery">Gallery</option>
                 <option value="settings">Settings</option>
             </select>
