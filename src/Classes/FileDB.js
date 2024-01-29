31cced63300a671dceb926ff5d2af71dea79ea91
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-01-29 07:41:36 +0100

    Update FileDB.js

--- src/Classes/FileDB.js
+++ src/Classes/FileDB.js
@@ -4,7 +4,7 @@ import Webserver from "./Webserver"
 class FileDB {
     constructor(Port) {
         this.Database = new Database(this, "./FileDB.sqlite")
-        this.Database.CreateTables().then(() => this.Database.CreateStatements())
+        this.Database.CreateTables()
 
         this.Webserver = new Webserver(Port, this)
         this.Webserver.Routes()
