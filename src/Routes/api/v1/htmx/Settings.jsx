fb58253403ae4298c6b500b105d620a4f5b71270
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-04 00:42:51 +0100

    SettingsMethod

---
+++ src/Routes/api/v1/htmx/Settings.jsx
@@ -0,0 +1,10 @@
+import Gallery from "../../../../Components/Gallery"
+
+export default function SettingsMethod(FileDB, App) {
+    App.get(
+        "/api/v1/htmx/settings",
+        async (Request) => {
+            return 
+        }
+    )
+}
