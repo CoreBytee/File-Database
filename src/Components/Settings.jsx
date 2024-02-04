0ca95e29a11b0ddbdc63a4687dd2a0988358dca0
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-04 22:18:43 +0100

    allow settings editing

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -1,15 +1,20 @@
 export default function Settings({ user }) {
     return (
         <div class="Settings maincontent">
-            <form class="floatingblock">
+            <form class="floatingblock" hx-post="/api/v1/htmx/settings" hx-swap="innerTEXT" hx-target=".error">
+                <h1>Settings</h1>
+                
                 <label htmlFor="usernamefield">Change username: </label><br />
-                <input type="text" name="username" id="usernamefield" value={user.Username}/><br /><br />
+                <input type="text" name="username" id="usernamefield" placeholder={user.Username} maxLength={30} /><br /><br />
 
                 <label htmlFor="emailfield">Change email: </label><br />
-                <input type="text" name="email" id="emailfield" value={user.Email} /><br /><br />
+                <input type="text" name="email" id="emailfield" placeholder={user.Email} maxLength={50} /><br /><br />
 
                 <label htmlFor="passwordfield">Change password: </label><br />
-                <input type="password" name="password" id="passwordfield" />
+                <input type="password" name="password" id="passwordfield" maxLength={512} /><br /><br />
+
+                <input type="submit" value="Save changes" /><br />
+                <p class="error"></p>
             </form>
         </div>
     )
--- src/Routes/api/v1/htmx/Settings.jsx
+++ src/Routes/api/v1/htmx/Settings.jsx
@@ -1,10 +1,36 @@
-import Gallery from "../../../../Components/Gallery"
+import CheckAuthentication from "../../../../Helpers/CheckAuthentication"
 
 export default function SettingsMethod(FileDB, App) {
-    App.get(
+    App.post(
         "/api/v1/htmx/settings",
         async (Request) => {
-            return 
+            console.log(Request.body)
+            const User = Request.User
+            if (Request.body.username === "" && Request.body.email === "" && Request.body.password === "") { return "No changes found" }
+            if (Request.body.username.length > 30 || Request.body.email.length > 50 || Request.body.password.length > 512) { return "Invalid input" }
+
+            console.log(User)
+
+            if (Request.body.username !== "") {
+                await User.SetUsername(Request.body.username)
+            }
+
+            if (Request.body.email !== "") {
+                await User.SetEmail(Request.body.email)
+            }
+
+            if (Request.body.password !== "") {
+                await User.SetPassword(Request.body.password)
+            }
+
+            Request.set.headers = {
+                "Hx-Refresh": "true"
+            }
+
+            return "ok"
+        },
+        {
+            beforeHandle: CheckAuthentication
         }
     )
 }
