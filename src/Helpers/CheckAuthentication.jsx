56e917a89a42d501d35c76bd4710f11bcae09e0e
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-06 09:42:54 +0100

    clean authentication code

--- src/Helpers/CheckAuthentication.jsx
+++ src/Helpers/CheckAuthentication.jsx
@@ -1,15 +1,16 @@
 import User from "../Classes/User"
 import LoginPage from "../Layouts/LoginPage"
 
+function ErrorResponse(Request) {
+    Request.set.status = 401
+    Request.set.headers["Content-Type"] = "text/html"
+    return <LoginPage />
+}
+
 export default async function CheckAuthentication(Request) {
     const AuthenticationCookie = Request.cookie.authentication.value
     const TokenData = await Request.JWT.verify(AuthenticationCookie)
-    if (!TokenData) {
-        console.log( "No Token Data" )
-        Request.set.status = 401
-        Request.set.headers["Content-Type"] = "text/html"
-        return <LoginPage />
-    } else {
-        Request.User = await User.FromId(TokenData.UserId)
-    }
+    if (!TokenData) { return ErrorResponse(Request) }
+    Request.User = await User.FromId(TokenData.UserId)
+    if (!Request.User) { return ErrorResponse(Request) }
 }
