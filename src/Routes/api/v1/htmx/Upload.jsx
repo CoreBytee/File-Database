9e5d18440ad9500732a29525c0e98d529e73670b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-06 13:21:46 +0100

    Update Upload.jsx

--- src/Routes/api/v1/htmx/Upload.jsx
+++ src/Routes/api/v1/htmx/Upload.jsx
@@ -6,10 +6,8 @@ export default function UploadMethod(FileDB, App) {
     App.post(
         "/api/v1/htmx/upload",
         async (Request) => {
-            console.log(Request.User)
             const FileData = Request.body.file
             const FileName = basename(Request.body.filename.replaceAll("\\", "/"))
-            console.log(File, FileName)
 
             const Upload = await File.Create(FileData, FileName, Request.User)
         },
