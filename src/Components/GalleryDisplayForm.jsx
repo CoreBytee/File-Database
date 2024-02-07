130cfb1b700e4f9cc7aebe7273a7a7632a3f7eff
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-07 22:24:49 +0100

    use labels

--- src/Components/GalleryDisplayForm.jsx
+++ src/Components/GalleryDisplayForm.jsx
@@ -2,17 +2,20 @@ export default function GalleryDisplayForm() {
     return (
         <div class="GalleryDisplayForm floatingblock">
             <form hx-get="/api/v1/htmx/gallery" hx-swap="outerHTML" hx-target=".Gallery" hx-trigger="load, change">
-                <p>Display as:</p>
-                <select name="displaytype" id="">
-                    <option value="largegrid">Large Grid</option>
-                    <option value="smallgrid">Small Grid</option>
-                    <option value="rows">Rows</option>
-                </select>
+                <label for="displaytypeselect">Display as:</label><br />
+                <select name="displaytype" id="displaytypeselect">
+                    <option value="LargeGrid">Large Grid</option>
+                    <option value="SmallGrid">Small Grid</option>
+                    <option value="Rows">Rows</option>
+                </select><br />
 
-                <p>Sort by:</p>
-                <select name="sortorder" id="">
-                    <option value="uploaddate">Upload date</option>
+                <label for="sortorderselect">Sort by:</label><br />
+                <select name="sortorder" id="sortorderselect">
+                    <option value="FileName">File name</option>
+                    <option value="Size">File size</option>
+                    <option value="UploadDate">Upload date</option>
                 </select>
+                <input type="checkbox" name="reversed" id="reversedcheckbox" />
             </form>
         </div>
     )
