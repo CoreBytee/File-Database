21d7774e9b2c7d1cc1492b80470a8606ad660012
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-09 23:40:01 +0100

    add rows

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,9 +1,22 @@
 import GalleryItem from "./GalleryItem";
 
 export default function Gallery({ Files = [], DisplayType = "LargeGrid" }) {
+    let Header
+    if (DisplayType == "Rows") {
+        Header = (
+            <div class="header">
+                <i class="bi bi-check2"></i>
+                <i class="bi bi-file-earmark"></i>
+                <p>Name</p>
+                <p>Size</p>
+                <p>Upload date</p>
+            </div>
+        )
+    }
     return (
         <div class="Gallery maincontent">
             <div class={DisplayType.toLowerCase()}>
+                {Header}
                 {Files.map(File => <GalleryItem File={File} DisplayType={DisplayType} />)}
             </div>
         </div>
--- src/Components/GalleryItem.jsx
+++ src/Components/GalleryItem.jsx
@@ -1,15 +1,27 @@
 export default function GalleryItem({ File, DisplayType }) {
-    return (
-        <div class={`GalleryItem floatingblock ${DisplayType.toLowerCase()}`}>
-            <input type="checkbox" name={File.Id} id="fileselect" hx-post="/api/v1/htmx/fileselection" hx-trigger="change" hx-include="#fileselect" />
-            <div class="contentcontainer">
-                {File.Render()}
+    if (DisplayType == "Rows") {
+        return (
+            <div class={`GalleryItem ${DisplayType.toLowerCase()}`}>
+                <input type="checkbox" name={File.Id} id="fileselect" hx-post="/api/v1/htmx/fileselection" hx-trigger="change" hx-include="#fileselect" />
                 <i class={`bi ${File.BootstrapIcon}`}></i>
+                <p>{File.Name}</p>
+                <p>{File.ReadableSize}</p>
+                <p>{File.ShortUploadDate}</p>
             </div>
-            <div class="data">
-                <p title={File.Name}>{File.Name}</p>
-                <p>{File.ReadableSize} {File.ShortUploadDate}</p>
+        )
+    } else {
+        return (
+            <div class={`GalleryItem floatingblock ${DisplayType.toLowerCase()}`}>
+                <input type="checkbox" name={File.Id} id="fileselect" hx-post="/api/v1/htmx/fileselection" hx-trigger="change" hx-include="#fileselect" />
+                <div class="contentcontainer">
+                    {File.Render()}
+                    <i class={`bi ${File.BootstrapIcon}`}></i>
+                </div>
+                <div class="data">
+                    <p title={File.Name}>{File.Name}</p>
+                    <p>{File.ReadableSize} {File.ShortUploadDate}</p>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
