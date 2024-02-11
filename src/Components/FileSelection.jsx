346a6166b8047342da21cdce7eb8b95a18623d4b
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2024-02-11 15:25:35 +0100

    dl button

--- src/Components/FileSelection.jsx
+++ src/Components/FileSelection.jsx
@@ -1,12 +1,14 @@
 import ReadableSize from "../Helpers/ReadableSize";
 
 export default function FileSelection({ Files = [] }) {
+    let RenderedFile
     let ViewLink
     let CopyLinkButton
     let DeleteButton
 
     if (Files.length == 1) {
-        ViewLink = <a href={Files[0].Link} target="_blank">View</a>
+        RenderedFile = Files[0].Render()
+        ViewLink = <><a href={Files[0].Link} target="_blank">View</a><a class={"button"} href={Files[0].RawLink}>Download</a></>
         CopyLinkButton = <button onClick={"navigator.clipboard.writeText(this.getAttribute('link'))"} link={Files[0].Link}>Copy Link</button>
     }
 
@@ -17,6 +19,7 @@ export default function FileSelection({ Files = [] }) {
     return (
         <div class="FileSelection floatingblock">
             <h1>Selected {Files.length} file{Files.length != 1 ? "s" : ""}</h1>
+            {RenderedFile}
             <p>Total size: {ReadableSize(Files.map(File => File.Size).reduce((Total, Size) => Total + Size, 0))}</p>
             {ViewLink}
             {CopyLinkButton}
